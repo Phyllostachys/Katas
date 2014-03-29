d13daebd8b108ec387f3652118e8a4bbf8236256
Jacob Shaffer <shaffer_jacob@hotmail.com>
2014-03-29 00:20:08 -0400

    added C# fizz buzz solution... it was much slower than C

---
+++ Code-Eval/easy/fizz_buzz.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    else
+                    {
+                        string[] splitLine = line.Trim().Split(' ');
+                        int m = int.Parse(splitLine[0]), n = int.Parse(splitLine[1]);
+                        for (int i = 1; i <= int.Parse(splitLine[2]); i++)
+                        {
+                            if (i % m == 0 && i % n == 0) {
+                                System.Console.Write("FB");
+                            } else if (i % m == 0) {
+                                System.Console.Write("F");
+                            } else if (i % n == 0) {
+                                System.Console.Write("B");
+                            } else {
+                                System.Console.Write(i);
+                            }
+                            System.Console.Write(" ");
+                        }
+                        System.Console.WriteLine();
+                    }
+                }
+        }
+    }
+}
