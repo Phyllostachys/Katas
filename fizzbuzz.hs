207ac5dda004dd3e39911ed6f853932d1ce551ae
Jacob Shaffer <shaffer_jacob@hotmail.com>
2015-01-08 11:55:24 -0500

    Update fizzbuzz.hs

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
-fizzbuzz :: (Show a, Integral a) => a -> [Char]
+fizzbuzz :: (Show a, Integral a) => a -> String
 fizzbuzz x
   = case (mod x 3 == 0, mod x 5 == 0) of
         (True, True) -> "FizzBuzz\n"
         (True, False) -> "Fizz\n"
         (False, True) -> "Buzz\n"
-        (False, False) -> (show x) ++ "\n"
+        (False, False) -> show x ++ "\n"
 
-flatten :: [[Char]] -> [Char]
+flatten :: [String] -> String
 flatten [] = []
-flatten (x : xs) = x ++ flatten xs
+flatten xs = concat xs
+
+main :: IO ()
 main = putStrLn . flatten $ map fizzbuzz [1 .. 100]
