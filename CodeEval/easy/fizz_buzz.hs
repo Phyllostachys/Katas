23fe8c66b6a39bf7948277fa4668c6d9acb3656f
Jacob Shaffer <shaffer_jacob@hotmail.com>
2015-02-03 17:13:17 -0500

    Update fizz_buzz.hs

--- CodeEval/easy/fizz_buzz.hs
+++ CodeEval/easy/fizz_buzz.hs
@@ -4,9 +4,9 @@ main :: IO ()
 main = do
     [inpFile] <- getArgs
     input <- readFile inpFile
-    mapM_ (putStrLn . unwords) $ map processInput $ map words $ lines input
+    mapM_ (((putStrLn . unwords) . processInput) . words) (lines input)
 
-processInput (x:y:z:[]) = map (fizzbuzz (read x) (read y)) [1..(read z)]
+processInput [x,y,z] = map (fizzbuzz (read x) (read y)) [1..(read z)]
 
 fizzbuzz :: Int -> Int -> Int -> String
 fizzbuzz x y n = case (mod n x == 0, mod n y == 0) of
